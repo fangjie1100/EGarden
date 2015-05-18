1743fd55b6078361ee20af7e5c95ab9c75f2c2d9
FangJie <fangjie1100@126.com>
2015-05-18 22:15:52 +0800

    gmail 测试完成

--- server/routes/sendmail.js
+++ server/routes/sendmail.js
@@ -2,8 +2,8 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var router = express.Router();
 
-var smtpTransport = nodemailer.createTransport('SMTP', {
-    service: 'Gmail',
+var smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
         user: 'fangjie1100@gmail.com',
         pass: 'fangjie123'
@@ -19,17 +19,17 @@ router.get('/', function (req, res, next) {
 
 router.put('/', function (req, res, next) {
     console.log(req);
-    //    var mailOptions = {
-    //        to: '349304351@qq.com',
-    //        subject: 'test email',
-    //        text: 'test email client'
-    //    };
-
     var mailOptions = {
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.body
+        form: 'fangjie1100@gmail.com',
+        bcc: '2196473880@qq.com',
+        to: '349304351@qq.com',
+        subject: 'test email',
+        text: 'test email client'
     };
+
+    mailOptions.to = req.body.to;
+    mailOptions.subject = req.body.subject;
+    mailOptions.text = req.body.body;
 
     console.log(mailOptions);
     smtpTransport.sendMail(mailOptions, function (error, response) {
