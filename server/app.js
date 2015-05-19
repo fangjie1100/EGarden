1814b46673a9c6c7abda7e8db1016574056cf3b8
fangjie <fangjie1100@126.com>
2015-05-20 00:43:34 +0800

    test qq login

--- server/app.js
+++ server/app.js
@@ -4,14 +4,62 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-ejs = require('ejs');
+var ejs = require('ejs');
+var passport = require('passport');
+var QQStrategy  = require('passport-qq').Strategy;
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new QQStrategy({
+    clientID: '101211799',
+    clientSecret: '5955ec423d2fd84d5a70020cbcbd6508',
+    callbackURL: 'http://visaandpassport.cn/callback'
+}, function (accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    process.nextTick(function () {
+        return done(null, profile);
+    });
+}));
+
+
+var app = express();
+app.use('/auth/qq',
+  passport.authenticate('qq'),
+  function(req, res){
+// The request will be redirected to qq for authentication, so this
+// function will not be called.
+});
+
+
+app.use('/callback', 
+  passport.authenticate('qq', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('login is ok');
+    res.redirect('/');
+  });
+
+app.use('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var classroom = require('./routes/class');
 var calendar = require('./routes/calendar');
 var calresult = require('./routes/calresult');
-var callback = require('./routes/callback');
+//var callback = require('./routes/callback');
 var login = require('./routes/login');
 var about = require('./routes/about');
 var register = require('./routes/register');
@@ -20,7 +68,10 @@ var testchat = require('./routes/testchat');
 var test = require('./routes/test');
 var sendMail = require('./routes/sendmail');
 
-var app = express();
+
+//app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, '/../public/views'));
@@ -44,7 +95,7 @@ app.use('/users', users);
 app.use('/class', classroom);
 app.use('/calendar', calendar);
 app.use('/calendarresult', calresult);
-app.use('/callback', callback);
+//app.use('/callback', callback);
 app.use('/login', login);
 app.use('/about', about);
 app.use('/register', register);
@@ -83,6 +134,11 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 
 module.exports = app;
